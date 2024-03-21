148e1a7f8589659aff24c87d36aa68ce62212704
Sanya Stepko <thisissony666@gmail.com>
2024-03-21 08:37:20 +0200

    added main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import requests
-import swapi
-
 from film_api import SWApi
-from print_film_info import print_film_info
+from print_film_info import print_film_info, numbersers
 
 
 api_client = SWApi()
-film = api_client.get_film(1)
+film = api_client.get_film(numbersers)
+
+
 print_film_info(film)
